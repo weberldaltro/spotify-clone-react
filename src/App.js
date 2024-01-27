0de8118431b0b3322a53aef7bcb3f146dad772d9
Weber Daltro <weda95@gmail.com>
2024-01-26 22:26:06 -0300

    adicionado sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Sidebar />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
