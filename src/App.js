17b7c1e6a441bdca1b99793b6e07b0700123ae4f
Weber Daltro <weda95@gmail.com>
2024-01-29 18:04:04 -0300

    adicionado Main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Sidebar />
       <Header />
+      <Main />
       <Footer />
     </div>
   );
