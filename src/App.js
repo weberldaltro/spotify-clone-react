181e222eecb3599c9b07b04ad5cb2aec3cbc7ea9
Weber Daltro <weda95@gmail.com>
2024-01-29 17:59:27 -0300

    adicionado footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Sidebar />
       <Header />
+      <Footer />
     </div>
   );
 }
